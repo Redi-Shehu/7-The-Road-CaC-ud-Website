e0ce4dca4c5bc6764fe755a15d9766eac2e9e273
redis <redshehu1@gmail.com>
2025-05-07 17:02:57 +0200

    Nav wrapper close when nav link click
    Small detail

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const container = document.querySelector(".container");
 const openNavIcon = document.querySelector(".open-navbar-icon");
 const navWrapper = document.querySelector(".navbar-wrapper");
 const closeNavIcon = document.querySelector(".close-navbar-icon");
+const navLinks = document.querySelectorAll(".nav-link");
 
 openNavIcon.addEventListener("click", (e) => {
   container.classList.add("change");
@@ -10,6 +11,13 @@ openNavIcon.addEventListener("click", (e) => {
 closeNavIcon.addEventListener("click", (e) => {
   container.classList.remove("change");
 });
+// close navWrapper when navLink clicked
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    container.classList.remove("change");
+  });
+});
+
 // Tours section
 const navButtons = document.querySelectorAll(".navigation-btn");
 
