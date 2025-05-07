c4d3a7283083be69c0a51045ff3f8b8c09b6251f
redis <redshehu1@gmail.com>
2025-05-07 16:11:17 +0200

    Navigation added

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Navigation Menu
+const container = document.querySelector(".container");
+const openNavIcon = document.querySelector(".open-navbar-icon");
+const navWrapper = document.querySelector(".navbar-wrapper");
+const closeNavIcon = document.querySelector(".close-navbar-icon");
+
+openNavIcon.addEventListener("click", (e) => {
+  container.classList.add("change");
+});
+closeNavIcon.addEventListener("click", (e) => {
+  container.classList.remove("change");
+});
+// Tours section
 const navButtons = document.querySelectorAll(".navigation-btn");
 
 navButtons.forEach((btn) => {
